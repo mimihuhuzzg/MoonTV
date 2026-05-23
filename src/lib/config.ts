ccb8645d14b5e9d48ef4a74b8b5d2e0ac9f8ec5f
48724 <48724@users.noreply.github.com>
2026-05-24 03:48:08 +0800

    fix: hardcode imageProxy fallback in config.ts

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -199,7 +199,9 @@ async function initConfig() {
             SearchDownstreamMaxPage:
               Number(process.env.NEXT_PUBLIC_SEARCH_MAX_PAGE) || 5,
             SiteInterfaceCacheTime: fileConfig.cache_time || 7200,
-            ImageProxy: process.env.NEXT_PUBLIC_IMAGE_PROXY || '',
+            ImageProxy:
+              process.env.NEXT_PUBLIC_IMAGE_PROXY ||
+              'https://improxy.zhangluotu1.dpdns.org/',
             DoubanProxy: process.env.NEXT_PUBLIC_DOUBAN_PROXY || '',
             DisableYellowFilter:
               process.env.NEXT_PUBLIC_DISABLE_YELLOW_FILTER === 'true',
@@ -247,7 +249,9 @@ async function initConfig() {
         SearchDownstreamMaxPage:
           Number(process.env.NEXT_PUBLIC_SEARCH_MAX_PAGE) || 5,
         SiteInterfaceCacheTime: fileConfig.cache_time || 7200,
-        ImageProxy: process.env.NEXT_PUBLIC_IMAGE_PROXY || '',
+        ImageProxy:
+          process.env.NEXT_PUBLIC_IMAGE_PROXY ||
+          'https://improxy.zhangluotu1.dpdns.org/',
         DoubanProxy: process.env.NEXT_PUBLIC_DOUBAN_PROXY || '',
         DisableYellowFilter:
           process.env.NEXT_PUBLIC_DISABLE_YELLOW_FILTER === 'true',
@@ -302,7 +306,8 @@ export async function getConfig(): Promise<AdminConfig> {
     adminConfig.UserConfig.AllowRegister =
       process.env.NEXT_PUBLIC_ENABLE_REGISTER === 'true';
     adminConfig.SiteConfig.ImageProxy =
-      process.env.NEXT_PUBLIC_IMAGE_PROXY || '';
+      process.env.NEXT_PUBLIC_IMAGE_PROXY ||
+      'https://improxy.zhangluotu1.dpdns.org/';
     adminConfig.SiteConfig.DoubanProxy =
       process.env.NEXT_PUBLIC_DOUBAN_PROXY || '';
     adminConfig.SiteConfig.DisableYellowFilter =
@@ -436,7 +441,9 @@ export async function resetConfig() {
       SearchDownstreamMaxPage:
         Number(process.env.NEXT_PUBLIC_SEARCH_MAX_PAGE) || 5,
       SiteInterfaceCacheTime: fileConfig.cache_time || 7200,
-      ImageProxy: process.env.NEXT_PUBLIC_IMAGE_PROXY || '',
+      ImageProxy:
+        process.env.NEXT_PUBLIC_IMAGE_PROXY ||
+        'https://improxy.zhangluotu1.dpdns.org/',
       DoubanProxy: process.env.NEXT_PUBLIC_DOUBAN_PROXY || '',
       DisableYellowFilter:
         process.env.NEXT_PUBLIC_DISABLE_YELLOW_FILTER === 'true',
